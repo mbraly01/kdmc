da135c46e1fa603ee05baf35383d16c9d4bff2d0
Matthew Braly <matthewbraly@matthews-laptop.lan>
2024-02-18 15:14:27 -0500

    First commit

---
+++ src/Card.js
@@ -0,0 +1,35 @@
+class Card {
+    constructor(text) {
+        this.text = text;
+    }
+
+    read_off(){
+        console.log(text);
+    }
+}
+
+class Hit_Location extends Card {
+    constructor(text) {
+        super(text);
+        const rules = text.split("$");
+    }
+}
+
+class Resources extends Card {
+    constructor(text) {
+        super(text);
+        const rules = text.split("$");
+        this.name = rules[0];
+        this.materials = rules[1].split(",");
+        this.bottom = rules[2];
+        this.consumable;
+        if (this.bottom.includes("consume")){
+            this.consumable = true;
+        } else {
+            this.consumable = false;
+        }
+    }
+}
+
+
+
